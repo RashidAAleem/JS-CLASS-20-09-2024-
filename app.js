b657b9c65e123cd8719be20cd4b8944cb5f36932
Rana Rashid <124416215+RashidAAleem@users.noreply.github.com>
2024-09-20 06:25:34 +0000

    done

--- app.js
+++ app.js
@@ -27,12 +27,46 @@
 // } 
 
 
-var isPallindrome = prompt("Please Enter a Word")
-var arrayvalues = isPallindrome.split("").reverse().join("");
-document.write(arrayvalues)
-if (arrayvalues === isPallindrome) { document.write(" <br> It is a Pallindrome")
+// var isPallindrome = prompt("Please Enter a Word")
+// var arrayvalues = isPallindrome.split("").reverse().join("");
+// document.write(arrayvalues)
+// if (arrayvalues === isPallindrome) { document.write(" <br> It is a Pallindrome")
     
-} else{
+// } else{
 
-    document.write("<br>It is Not a Pallindrome")
+//     document.write("<br>It is Not a Pallindrome")
+// }
+
+// var isPallindrome = prompt("Please Enter a Word")
+// var arrayvalues = isPallindrome.split("").reverse().join("");
+// document.write(arrayvalues)
+// if (arrayvalues === isPallindrome) { document.write(" <br> It is a Pallindrome")
+    
+// } else{
+
+//     document.write("<br>It is Not a Pallindrome")
+// }
+
+
+
+
+
+var word = prompt ("Please Enter a word");
+var newword = ""
+document.write("Input Word: " + word + "<br>")
+
+for (let i = word.length-1; i >=0; i--) {
+    newword += word[i]
+
 }
+document.write("Reverse Word: " + newword)
+
+    if (newword=== word) { 
+        document.write("<br> It is a Pallindrome")
+        
+    }else{
+
+        document.write("<br> It is a Not a Pallindrome")
+    } 
+
+
